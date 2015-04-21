0f18ca3885202f3d8773cc834af3debd1e9769fa
Jacek Przemieniecki <jacek@przemieniecki.net>
2015-04-21 11:49:36 +0200

    Add Speedometer

---
+++ UnityProject/Assets/Scripts/Speedometer.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class Speedometer : MonoBehaviour
+{
+	private Rigidbody _car;
+	private Text _text;
+
+	private void Awake()
+	{
+		_text = GetComponent<Text>();
+		_car = GameObject.FindWithTag("Player").GetComponent<Rigidbody>();
+	}
+
+	void Update()
+	{
+		_text.text = (Vector3.Dot(_car.velocity, _car.transform.forward)*3.6f).ToString("0.0");
+	}
+}
