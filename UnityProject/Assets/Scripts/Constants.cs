ce5477fac077d72838aa0540b584618b2d0f1bb4
Jacek Przemieniecki <jacek@przemieniecki.net>
2015-05-05 21:24:26 +0200

    High scores

--- UnityProject/Assets/Scripts/Constants.cs
+++ UnityProject/Assets/Scripts/Constants.cs
@@ -1,4 +1,5 @@
 ﻿public static class Constants
 {
 	public const string PlayerCarTag = "Car";
+	public const int CheckpointCount = 6;
 }
--- UnityProject/Assets/Scripts/RaceLogic/Timer.cs
+++ UnityProject/Assets/Scripts/RaceLogic/Timer.cs
@@ -3,21 +3,43 @@ using UnityEngine.UI;
 
 public class Timer : MonoBehaviour
 {
-	private float _timeStart;
+	private const string BestTimeKey = "BestTime";
+	private const string CheckpointTimePrefix = "Time";
+	private readonly float[] _bestTimes = new float[Constants.CheckpointCount];
+	private float _bestLapTime;
+	private bool _inRace;
 	private float _timeLastCheckpoint;
-
-	public Text SinceStart;
+	private float _timeStart;
+	public Text BestCheckpoint;
+	public Text BestTotal;
 	public Text SinceLastCheckpoint;
+	public Text SinceStart;
 
-	private bool _inRace = false;
-
-	void Start()
+	private void Start()
 	{
 		Checkpoint.CheckPointReached += OnCheckpointReached;
 		LevelManager.RaceStarted += OnRaceStarted;
 		LevelManager.RaceFinished += OnRaceFinished;
 		SinceStart.text = "0.0";
 		SinceLastCheckpoint.text = "0.0";
+		if (PlayerPrefs.HasKey(BestTimeKey))
+		{
+			_bestLapTime = PlayerPrefs.GetFloat(BestTimeKey);
+			for (var i = 0; i < Constants.CheckpointCount; i++)
+			{
+				_bestTimes[i] = PlayerPrefs.GetFloat(CheckpointTimePrefix + i);
+			}
+		}
+		else
+		{
+			_bestLapTime = Mathf.Infinity;
+			for (var i = 0; i < Constants.CheckpointCount; i++)
+			{
+				_bestTimes[i] = Mathf.Infinity;
+			}
+		}
+		BestTotal.text = _bestLapTime.ToString("0.0");
+		BestCheckpoint.text = _bestTimes[0].ToString("0.0");
 	}
 
 	private void Update()
@@ -27,8 +49,8 @@ public class Timer : MonoBehaviour
 			return;
 		}
 
-		float timeSinceStart = (Time.time - _timeStart);
-		float timeSinceCheckpoint = (Time.time - _timeLastCheckpoint);
+		var timeSinceStart = Time.time - _timeStart;
+		var timeSinceCheckpoint = Time.time - _timeLastCheckpoint;
 
 		SinceStart.text = timeSinceStart.ToString("0.0");
 		SinceLastCheckpoint.text = timeSinceCheckpoint.ToString("0.0");
@@ -41,21 +63,45 @@ public class Timer : MonoBehaviour
 		_inRace = true;
 	}
 
-	void OnCheckpointReached(int checkpoint)
+	private void OnCheckpointReached(int checkpoint)
 	{
+		var timeSinceLastCheckpoint = Time.time - _timeLastCheckpoint;
+		Debug.Log(timeSinceLastCheckpoint);
+		if (timeSinceLastCheckpoint < _bestTimes[checkpoint])
+		{
+			_bestTimes[checkpoint] = timeSinceLastCheckpoint;
+		}
+		if (checkpoint + 1 < Constants.CheckpointCount)
+		{
+			BestCheckpoint.text = _bestTimes[checkpoint + 1].ToString("0.0");
+		}
+
 		_timeLastCheckpoint = Time.time;
 	}
 
-	void OnRaceFinished()
+	private void OnRaceFinished()
 	{
 		_inRace = false;
+
+		var lapTime = Time.time - _timeStart;
+
+		if (lapTime < _bestLapTime)
+		{
+			PlayerPrefs.SetFloat(BestTimeKey, lapTime);
+		}
+
+		Debug.Log("Writing best times");
+		for (var i = 0; i < Constants.CheckpointCount; i++)
+		{
+			Debug.Log(_bestTimes[i]);
+			PlayerPrefs.SetFloat(CheckpointTimePrefix + i, _bestTimes[i]);
+		}
 	}
 
-	void OnDestroy()
+	private void OnDestroy()
 	{
 		Checkpoint.CheckPointReached -= OnCheckpointReached;
 		LevelManager.RaceStarted -= OnRaceStarted;
 		LevelManager.RaceFinished -= OnRaceFinished;
 	}
 }
-
