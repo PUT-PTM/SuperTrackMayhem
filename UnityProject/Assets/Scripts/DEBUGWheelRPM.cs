097d1925db04fb9719dd1d57c0ac2ce7b3c25195
Jacek Przemieniecki <jacek@przemieniecki.net>
2015-06-07 18:35:56 +0200

    Higher acceleration

---
+++ UnityProject/Assets/Scripts/DEBUGWheelRPM.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class DEBUGWheelRPM : MonoBehaviour
+{
+    private WheelCollider _wheelCollider;
+
+    void Awake()
+    {
+        _wheelCollider = GetComponent<WheelCollider>();
+    }
+
+    void FixedUpdate()
+    {
+        Debug.Log("--------------------");
+        Debug.Log("RPM: " + _wheelCollider.rpm);
+        WheelHit hit;
+        _wheelCollider.GetGroundHit(out hit);
+        Debug.Log("Forward slip: " + hit.forwardSlip / _wheelCollider.forwardFriction.extremumSlip);
+        Debug.Log("Side slip: " + hit.sidewaysSlip / _wheelCollider.sidewaysFriction.extremumSlip);
+    }
+}
