7c61dc5ede5f0e4c89f589163b064c7de0037599
Jacek Przemieniecki <jacek@przemieniecki.net>
2015-06-10 00:07:10 +0200

    Reformat

--- UnityProject/Assets/Scripts/UI/StartingLights.cs
+++ UnityProject/Assets/Scripts/UI/StartingLights.cs
@@ -3,24 +3,23 @@ using UnityEngine;
 
 public class StartingLights : MonoBehaviour
 {
-	public float TimeBetweenLights;
+    public GameObject[] Lights;
+    public float TimeBetweenLights;
 
-	public GameObject[] Lights;
+    private void Start()
+    {
+        StartCoroutine(LightsSequence());
+    }
 
-	private void Start()
-	{
-		StartCoroutine(LightsSequence());
-	}
-
-	private IEnumerator LightsSequence()
-	{
-		for (int i = 0; i < Lights.Length; i++)
-		{
-			yield return new WaitForSeconds(TimeBetweenLights);
-			Lights[i].SetActive(true);
-		}
-		LevelManager.OnStartRace();
-		yield return new WaitForSeconds(TimeBetweenLights);
-		gameObject.SetActive(false);
-	}
+    private IEnumerator LightsSequence()
+    {
+        for (var i = 0; i < Lights.Length; i++)
+        {
+            yield return new WaitForSeconds(TimeBetweenLights);
+            Lights[i].SetActive(true);
+        }
+        LevelManager.StartRace();
+        yield return new WaitForSeconds(TimeBetweenLights);
+        gameObject.SetActive(false);
+    }
 }
