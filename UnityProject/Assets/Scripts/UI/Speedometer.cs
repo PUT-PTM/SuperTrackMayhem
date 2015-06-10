1432dd4e875161ec78a99617f26d20426e341e5b
Jacek Przemieniecki <jacek@przemieniecki.net>
2015-06-10 14:23:57 +0200

    Speedometer

--- UnityProject/Assets/Scripts/UI/Speedometer.cs
+++ UnityProject/Assets/Scripts/UI/Speedometer.cs
@@ -1,20 +1,21 @@
 ﻿using UnityEngine;
-using UnityEngine.UI;
 
-[RequireComponent(typeof(Text))]
 public class Speedometer : MonoBehaviour
 {
-	private Rigidbody _car;
-	private Text _text;
+    private Rigidbody _car;
+    public Transform Hand;
+    public float Rotation120;
 
-	private void Awake()
-	{
-		_text = GetComponent<Text>();
-		_car = GameObject.FindWithTag("Player").GetComponent<Rigidbody>();
-	}
+    private void Awake()
+    {
+        _car = GameObject.FindWithTag("Player").GetComponent<Rigidbody>();
+    }
 
-	void Update()
-	{
-		_text.text = (Vector3.Dot(_car.velocity, _car.transform.forward)*3.6f).ToString("0.0");
-	}
+    private void Update()
+    {
+        var speed = Vector3.Dot(_car.velocity, _car.transform.forward)*3.6f;
+        // The speed is in m/s even though the speedometer says mph. 
+        // This is intentional since our car runs at unrealistic speeds.
+        Hand.localRotation = Quaternion.AngleAxis(speed/120*Rotation120, Vector3.forward);
+    }
 }
